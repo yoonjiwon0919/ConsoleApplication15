739c39e58fe331b858a9f44111242bf7e963e265
kimwoobin0919 <kimwoobin0919@outlook.kr>
2020-06-09 03:15:20 +0900

    초기 커밋.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[7] = { 1, 4, 3, 2, 5, 7, 6 };
+    int* front = &arr[0];
+    int* back = &arr[6];
+
+    for (int i = 0; i < 3; i++)
+    {
+        int a = arr[i];
+        *(front + i) = *(back - i);
+        *(back - i) = a;
+    }
+
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
